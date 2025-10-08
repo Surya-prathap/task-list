ffc7e6430f59791e7626b1b9661b6e1542d48d6d
Surya Prathap Reddy <13794prathap@gmail.com>
2025-10-08 09:06:09 +0530

    cors error fixed

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,14 @@ const dotEnv = require("dotenv").config();
 
 const PORT = 8000;
 
+const corsOptions = {
+  origin: "https://task-list-c43x.vercel.app/",
+  optionSuccessStatus: 200,
+};
+
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose
