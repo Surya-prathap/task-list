d2a191b2775549d7e8d528e2eb8c83a2ada19923
Surya Prathap Reddy <13794prathap@gmail.com>
2025-10-08 11:15:21 +0530

    added changes

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,14 @@ const cors = require("cors");
 const dotEnv = require("dotenv").config();
 
 const PORT = 8000;
+const corsOptions = {
+  origin: "https://task-list-frontend-khaki.vercel.app/",
+  optionSuccessStatus: 200,
+};
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose
